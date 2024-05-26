950f016c7ffe00c11f2ba3cb0e53e9c9044cb147
Marc Walter <walter.marc@outlook.com>
2024-05-26 20:51:25 +0200

    The WASM backend does not suport quasiquotes and template Haskell

--- compiler/src/Generate/JavaScript/Functions.hs
+++ compiler/src/Generate/JavaScript/Functions.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Generate.JavaScript.Functions
   ( functions
   )
@@ -6,90 +6,88 @@ module Generate.JavaScript.Functions
 
 
 import qualified Data.ByteString.Builder as B
-import Text.RawString.QQ (r)
 
-
+-- import Text.RawString.QQ (r)
 
 -- FUNCTIONS
 
+-- JS backend does not support TemplateHaskell and QuasiQuotes, need to write it is a multiline string
 
 functions :: B.Builder
-functions = [r|
-
-function F(arity, fun, wrapper) {
-  wrapper.a = arity;
-  wrapper.f = fun;
-  return wrapper;
-}
-
-function F2(fun) {
-  return F(2, fun, function(a) { return function(b) { return fun(a,b); }; })
-}
-function F3(fun) {
-  return F(3, fun, function(a) {
-    return function(b) { return function(c) { return fun(a, b, c); }; };
-  });
-}
-function F4(fun) {
-  return F(4, fun, function(a) { return function(b) { return function(c) {
-    return function(d) { return fun(a, b, c, d); }; }; };
-  });
-}
-function F5(fun) {
-  return F(5, fun, function(a) { return function(b) { return function(c) {
-    return function(d) { return function(e) { return fun(a, b, c, d, e); }; }; }; };
-  });
-}
-function F6(fun) {
-  return F(6, fun, function(a) { return function(b) { return function(c) {
-    return function(d) { return function(e) { return function(f) {
-    return fun(a, b, c, d, e, f); }; }; }; }; };
-  });
-}
-function F7(fun) {
-  return F(7, fun, function(a) { return function(b) { return function(c) {
-    return function(d) { return function(e) { return function(f) {
-    return function(g) { return fun(a, b, c, d, e, f, g); }; }; }; }; }; };
-  });
-}
-function F8(fun) {
-  return F(8, fun, function(a) { return function(b) { return function(c) {
-    return function(d) { return function(e) { return function(f) {
-    return function(g) { return function(h) {
-    return fun(a, b, c, d, e, f, g, h); }; }; }; }; }; }; };
-  });
-}
-function F9(fun) {
-  return F(9, fun, function(a) { return function(b) { return function(c) {
-    return function(d) { return function(e) { return function(f) {
-    return function(g) { return function(h) { return function(i) {
-    return fun(a, b, c, d, e, f, g, h, i); }; }; }; }; }; }; }; };
-  });
-}
-
-function A2(fun, a, b) {
-  return fun.a === 2 ? fun.f(a, b) : fun(a)(b);
-}
-function A3(fun, a, b, c) {
-  return fun.a === 3 ? fun.f(a, b, c) : fun(a)(b)(c);
-}
-function A4(fun, a, b, c, d) {
-  return fun.a === 4 ? fun.f(a, b, c, d) : fun(a)(b)(c)(d);
-}
-function A5(fun, a, b, c, d, e) {
-  return fun.a === 5 ? fun.f(a, b, c, d, e) : fun(a)(b)(c)(d)(e);
-}
-function A6(fun, a, b, c, d, e, f) {
-  return fun.a === 6 ? fun.f(a, b, c, d, e, f) : fun(a)(b)(c)(d)(e)(f);
-}
-function A7(fun, a, b, c, d, e, f, g) {
-  return fun.a === 7 ? fun.f(a, b, c, d, e, f, g) : fun(a)(b)(c)(d)(e)(f)(g);
-}
-function A8(fun, a, b, c, d, e, f, g, h) {
-  return fun.a === 8 ? fun.f(a, b, c, d, e, f, g, h) : fun(a)(b)(c)(d)(e)(f)(g)(h);
-}
-function A9(fun, a, b, c, d, e, f, g, h, i) {
-  return fun.a === 9 ? fun.f(a, b, c, d, e, f, g, h, i) : fun(a)(b)(c)(d)(e)(f)(g)(h)(i);
-}
-
-|]
+functions =
+  "\n\n\
+  \function F(arity, fun, wrapper) {\n\
+  \  wrapper.a = arity;\n\
+  \  wrapper.f = fun;\n\
+  \  return wrapper;\n\
+  \}\n\
+  \\n\
+  \function F2(fun) {\n\
+  \  return F(2, fun, function(a) { return function(b) { return fun(a,b); }; })\n\
+  \}\n\
+  \function F3(fun) {\n\
+  \  return F(3, fun, function(a) {\n\
+  \    return function(b) { return function(c) { return fun(a, b, c); }; };\n\
+  \  });\n\
+  \}\n\
+  \function F4(fun) {\n\
+  \  return F(4, fun, function(a) { return function(b) { return function(c) {\n\
+  \    return function(d) { return fun(a, b, c, d); }; }; };\n\
+  \  });\n\
+  \}\n\
+  \function F5(fun) {\n\
+  \  return F(5, fun, function(a) { return function(b) { return function(c) {\n\
+  \    return function(d) { return function(e) { return fun(a, b, c, d, e); }; }; }; };\n\
+  \  });\n\
+  \}\n\
+  \function F6(fun) {\n\
+  \  return F(6, fun, function(a) { return function(b) { return function(c) {\n\
+  \    return function(d) { return function(e) { return function(f) {\n\
+  \    return fun(a, b, c, d, e, f); }; }; }; }; };\n\
+  \  });\n\
+  \}\n\
+  \function F7(fun) {\n\
+  \  return F(7, fun, function(a) { return function(b) { return function(c) {\n\
+  \    return function(d) { return function(e) { return function(f) {\n\
+  \    return function(g) { return fun(a, b, c, d, e, f, g); }; }; }; }; }; };\n\
+  \  });\n\
+  \}\n\
+  \function F8(fun) {\n\
+  \  return F(8, fun, function(a) { return function(b) { return function(c) {\n\
+  \    return function(d) { return function(e) { return function(f) {\n\
+  \    return function(g) { return function(h) {\n\
+  \    return fun(a, b, c, d, e, f, g, h); }; }; }; }; }; }; };\n\
+  \  });\n\
+  \}\n\
+  \function F9(fun) {\n\
+  \  return F(9, fun, function(a) { return function(b) { return function(c) {\n\
+  \    return function(d) { return function(e) { return function(f) {\n\
+  \    return function(g) { return function(h) { return function(i) {\n\
+  \    return fun(a, b, c, d, e, f, g, h, i); }; }; }; }; }; }; }; };\n\
+  \  });\n\
+  \}\n\
+  \\n\
+  \function A2(fun, a, b) {\n\
+  \  return fun.a === 2 ? fun.f(a, b) : fun(a)(b);\n\
+  \}\n\
+  \function A3(fun, a, b, c) {\n\
+  \  return fun.a === 3 ? fun.f(a, b, c) : fun(a)(b)(c);\n\
+  \}\n\
+  \function A4(fun, a, b, c, d) {\n\
+  \  return fun.a === 4 ? fun.f(a, b, c, d) : fun(a)(b)(c)(d);\n\
+  \}\n\
+  \function A5(fun, a, b, c, d, e) {\n\
+  \  return fun.a === 5 ? fun.f(a, b, c, d, e) : fun(a)(b)(c)(d)(e);\n\
+  \}\n\
+  \function A6(fun, a, b, c, d, e, f) {\n\
+  \  return fun.a === 6 ? fun.f(a, b, c, d, e, f) : fun(a)(b)(c)(d)(e)(f);\n\
+  \}\n\
+  \function A7(fun, a, b, c, d, e, f, g) {\n\
+  \  return fun.a === 7 ? fun.f(a, b, c, d, e, f, g) : fun(a)(b)(c)(d)(e)(f)(g);\n\
+  \}\n\
+  \function A8(fun, a, b, c, d, e, f, g, h) {\n\
+  \  return fun.a === 8 ? fun.f(a, b, c, d, e, f, g, h) : fun(a)(b)(c)(d)(e)(f)(g)(h);\n\
+  \}\n\
+  \function A9(fun, a, b, c, d, e, f, g, h, i) {\n\
+  \  return fun.a === 9 ? fun.f(a, b, c, d, e, f, g, h, i) : fun(a)(b)(c)(d)(e)(f)(g)(h)(i);\n\
+  \}\n\n"
